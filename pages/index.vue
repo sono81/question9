d41da0c4c5a4d837c646c8437b9cc8de020b1744
Sondre N <49595556+sono81@users.noreply.github.com>
2020-10-23 11:57:23 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        Question9
+      </h1>
+      <div class="links">
+        <NuxtLink to="/page2"
+          class="button--green"
+        >
+          Proceed
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #4E3466;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/page2.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Question 9
+      </h1>
+      <div class="text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid cum
+        minima maxime distinctio ipsam sequi impedit hic laudantium cumque
+        libero doloremque omnis explicabo itaque odit perferendis, laboriosam
+        possimus repellendus provident?
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.text {
+    width: 80%;
+    text-align: left;
+    margin: 0 auto;
+}
+</style>
