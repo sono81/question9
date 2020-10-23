bcd3cbebd99183a62828107ea6fe5a48ab415d74
Sondre N <49595556+sono81@users.noreply.github.com>
2020-10-23 12:45:19 +0200

    add list of instructions

--- pages/page2.vue
+++ pages/page2.vue
@@ -4,11 +4,25 @@
       <h1 class="title">
         Question 9
       </h1>
+      <div class="subtitle">
+          Steps:
+      </div>
       <div class="text">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid cum
-        minima maxime distinctio ipsam sequi impedit hic laudantium cumque
-        libero doloremque omnis explicabo itaque odit perferendis, laboriosam
-        possimus repellendus provident?
+        <ol>
+            <li>I created a nuxt app with the command "npx create-nuxt-app question9"</li>
+            <li>Created a basic logo in Illustrator, saved as .svg so i could play around with some animation in CSS to give the app a bit more personallity</li>
+            <li>Edited the index.vue file to serve my needs.</li>
+            <li>Added a new page "page2.vue"</li>
+            <li>Created a new Git repo and pushed my content before making any new changes to the app</li>
+            <li>Deployed my Git repo to netlify</li>
+        </ol>
+      </div>
+      <div class="links">
+            <NuxtLink to="/"
+                class="button--green"
+            >
+                Back
+            </NuxtLink>
       </div>
     </div>
   </div>
@@ -23,5 +37,9 @@ export default {};
     width: 80%;
     text-align: left;
     margin: 0 auto;
+}
+
+li {
+    margin-bottom: 15px;
 }
 </style>
