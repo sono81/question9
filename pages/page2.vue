087416c559e0db481d5d48c08ae1b30aabe55287
Sondre N <49595556+sono81@users.noreply.github.com>
2020-10-23 13:11:44 +0200

    add list items

--- pages/page2.vue
+++ pages/page2.vue
@@ -9,12 +9,13 @@
       </div>
       <div class="text">
         <ol>
-            <li>I created a nuxt app with the command "npx create-nuxt-app question9"</li>
-            <li>Created a basic logo in Illustrator, saved as .svg so i could play around with some animation in CSS to give the app a bit more personallity</li>
+            <li>I created a nuxt app with the command "npx create-nuxt-app question9".</li>
+            <li>Created a basic logo in Illustrator, saved as .svg so i could play around with some animation in CSS to give the app a bit more personallity.</li>
             <li>Edited the index.vue file to serve my needs.</li>
-            <li>Added a new page "page2.vue"</li>
-            <li>Created a new Git repo and pushed my content before making any new changes to the app</li>
-            <li>Deployed my Git repo to netlify</li>
+            <li>Added a new page "page2.vue".</li>
+            <li>Created a new Git repo and pushed my content before making any new changes to the app.</li>
+            <li>Deployed my Git repo to netlify.</li>
+            <li>Added final preperations to the app.</li>
         </ol>
       </div>
       <div class="links">
